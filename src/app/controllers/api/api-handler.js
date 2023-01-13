fb012627f1d696174f5be6de3249ec20df7b6d5f
Scynes <dustin.greyfield@gmail.com>
2023-01-13 18:13:47 -0500

    wip: added "abstract" methods for inheritance implementation

---
+++ src/app/controllers/api/api-handler.js
@@ -0,0 +1,25 @@
+import Mongoose from 'mongoose';
+
+export class APIHandler {
+
+    constructor (model) {
+
+    }
+
+    handleCreate = (request, response) => {
+
+    }
+
+    handleRead = (request, response) => {
+
+    }
+
+    handleUpdate = (request, response) => {
+
+    }
+
+    handleDelete = (request, response) => {
+        
+    }
+
+}
