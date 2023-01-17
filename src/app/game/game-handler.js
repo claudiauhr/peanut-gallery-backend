45d71720fc36928724220fb9a237e06f38155b14
Scynes <dustin.greyfield@gmail.com>
2023-01-17 13:32:56 -0500

    wip: added getGameSession(id) getter

--- src/app/game/game-handler.js
+++ src/app/game/game-handler.js
@@ -23,3 +23,16 @@ export const createGameSession = (id) => {
 
     return (GAME_SESSIONS[id] = CREATED_GAME);
 }
+
+/**
+ * Getter for grabbing a game session. If the session id does not
+ * exist, a null value will be returned. This method must be type checked
+ * where implemented to avoid application crashes for undefined references.
+ * 
+ * @param {*} id the game session identifier.
+ * @returns {Trivia}
+ */
+export const getGameSession = (id) => {
+
+    return (!(id in GAME_SESSIONS) ? null : GAME_SESSIONS[id]);
+}
