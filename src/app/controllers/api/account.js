0e375ea73d410a614831ecab52fd64bd1bf534a5
Scynes <dustin.greyfield@gmail.com>
2023-01-18 11:42:03 -0500

    compelte/wip: added password hashing for account creation

--- src/app/controllers/api/account.js
+++ src/app/controllers/api/account.js
@@ -1,9 +1,14 @@
 import { APIHandler } from "./api-handler.js";
 import { ACCOUNT_MODEL } from "../../models/account-schema.js";
+import { generateHashed, matchesHashed } from '../../utils/hashing.js'
 
 class Account extends APIHandler {
 
     handleCreate = async (request, response) => {
+
+        //TODO need to implement error handling!
+
+        request.body.password = await generateHashed(request.body.password);
         
         const NEW_ACCOUNT = await this.create(request.body);
 
--- src/app/utils/hashing.js
+++ src/app/utils/hashing.js
@@ -1,27 +1,24 @@
 import Hasher from 'bcrypt';
 
-module.exports = {
+/**
+ * Returns a hashed string using bcrypt of the input passed.
+ * 
+ * @param {*} input
+ */
+export const generateHashed = input => {
 
-    /**
-     * Returns a hashed string using bcrypt of the input passed.
-     * 
-     * @param {*} input
-     */
-    generate: input => {
+    return Hasher.hash(input, 10);
+}
 
-        return Hasher.hash(input, 10);
-    },
+/**
+ * Returns a boolean state if the input string hashed matches
+ * the cimparable against hash.
+ * 
+ * @param {*} input 
+ * @param {*} against 
+ * @returns 
+ */
+export const matchesHashed = (input, against) => {
 
-    /**
-     * Returns a boolean state if the input string hashed matches
-     * the cimparable against hash.
-     * 
-     * @param {*} input 
-     * @param {*} against 
-     * @returns 
-     */
-    matches: (input, against) => {
-
-        return Hasher.compare(input, against);
-    }
+    return Hasher.compare(input, against);
 }
