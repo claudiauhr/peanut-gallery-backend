6b7e9186866530ff09e7e479354ae62ae60e6c0f
audemusprime <edbrossa@gmail.com>
2023-01-17 19:02:47 -0500

    adds authentication and authorized routes

--- src/app/routes/create.js
+++ src/app/routes/create.js
@@ -1,6 +1,8 @@
 import Express from "express";
 import { APIHandler } from "../controllers/api/api-handler.js";
 import Trivia from "../controllers/api/trivia.js";
+import User_Schema from "../controllers/api/auth.js"
+import isAuth from '../middleware/authenticated.js'
 
 /**
  * Defines the create(post) router reference.
@@ -13,7 +15,8 @@ const CREATE_ROUTER = Express.Router();
 export const ROUTES = () => {
 
     CREATE_ROUTER.post('/', APIHandler.handleDefault);
-    CREATE_ROUTER.post('/trivia', Trivia.handleCreate);
+    CREATE_ROUTER.post('/trivia', isAuth, Trivia.handleCreate);
+    CREATE_ROUTER.post('/register', User_Schema.handleCreate)
 
     return CREATE_ROUTER;
 }
