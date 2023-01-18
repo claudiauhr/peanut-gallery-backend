0c751718504ed07f0785d87ca49c27605b98ce09
Scynes <dustin.greyfield@gmail.com>
2023-01-18 11:23:58 -0500

    wip: added beta account model

---
+++ src/app/controllers/api/account.js
@@ -0,0 +1,22 @@
+import { APIHandler } from "./api-handler.js";
+
+class Account extends APIHandler {
+
+    handleCreate = (request, response) => {
+
+        
+    }
+
+    handleRead = (request, response) => {
+
+    }
+
+    handleDelete = (request, response) => {
+
+    }
+
+    handleUpdate = (request, response) => {
+
+    }
+
+}
---
+++ src/app/models/account-schema.js
@@ -0,0 +1,28 @@
+import Mongoose from 'mongoose';
+
+/**
+ * Holds the Schema reference for mongoose.
+ */
+const Schema = Mongoose.Schema;
+
+/**
+ * MongoDB document model schema.
+ */
+const ACCOUNT_SCHEMA = new Schema({
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+
+}, { timestamps: true, collection: 'accounts' });
+
+/**
+ * Exports the schema model for use in APIHandler's.
+ */
+export const ACCOUNT_MODEL = Mongoose.model('peanutGallery', ACCOUNT_SCHEMA);
